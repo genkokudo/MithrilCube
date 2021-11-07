fff32c9375280ee2ce515faa29be0fe635c18316
ginpay <y-toyoda@live.jp>
2021-11-07 22:10:47 +0900

    指定フォルダを再帰的に削除。

--- MithrilCube/Services/DirectoryService.cs
+++ MithrilCube/Services/DirectoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,6 +29,12 @@ namespace MithrilCube.Services
         /// <param name="extensions">検索する拡張子群{ ".cs", ".exe"}みたいな感じ</param>
         public void FolderInsiteSearch(string folderPath, List<string> fileFullPathList, string[] extensions);
 
+        /// <summary>
+        /// 指定したディレクトリとその中身を全て削除する
+        /// </summary>
+        /// <param name="directory">ディレクトリ</param>
+        public void DeleteDirectory(string directory);
+
         ///// <summary>
         ///// ファイルを対象のディレクトリにコピーします
         ///// ディレクトリが無ければ作成します
@@ -56,13 +63,6 @@ namespace MithrilCube.Services
             return Directory.CreateDirectory(directory);
         }
 
-        /// <summary>
-        /// 指定フォルダ以下のすべてのフォルダを探索し、
-        /// 指定拡張子のファイル名をリストに順次追加していく
-        /// </summary>
-        /// <param name="folderPath">探索するフォルダ</param>
-        /// <param name="list">ファイル名のリスト</param>
-        /// <param name="extensions">検索する拡張子群{ ".cs", ".exe"}みたいな感じ</param>
         public void FolderInsiteSearch(string folderPath, List<string> filenameList, string[] extensions)
         {
             //現在のフォルダ内の指定拡張子のファイル名をリストに追加
@@ -79,5 +79,54 @@ namespace MithrilCube.Services
                 foreach (var dirName in dirNames)
                     FolderInsiteSearch(dirName, filenameList, extensions);
         }
+
+        public void DeleteDirectory(string directory)
+        {
+            // 再帰処理
+            DeleteDirectory(directory, true);
+        }
+
+        // 外から呼び出すときはisTop=trueとする
+        private void DeleteDirectory(string directory, bool isTop)
+        {
+            if (!Directory.Exists(directory))
+            {
+                return;
+            }
+
+            //ディレクトリ以外の全ファイルを削除
+            string[] filePaths = Directory.GetFiles(directory);
+            foreach (string filePath in filePaths)
+            {
+                File.SetAttributes(filePath, FileAttributes.Normal);
+                File.Delete(filePath);
+            }
+
+            //ディレクトリの中のディレクトリも再帰的に削除
+            string[] directoryPaths = Directory.GetDirectories(directory);
+            foreach (string directoryPath in directoryPaths)
+            {
+                DeleteDirectory(directoryPath, false);
+            }
+
+            if (!isTop)
+            {
+                //中が空になったらディレクトリ自身も削除
+                Directory.Delete(directory, false);
+            }
+        }
+
+        ///// <summary>
+        ///// 指定したディレクトリの中身を全て削除する
+        ///// （更に中身のあるフォルダがある場合の動作を確認してないので没）
+        ///// </summary>
+        //public void DeleteFiles(string directory)
+        //{
+        //    DirectoryInfo target = new DirectoryInfo(directory);
+        //    foreach (FileInfo file in target.GetFiles())
+        //    {
+        //        file.Delete();
+        //    }
+        //}
     }
 }
