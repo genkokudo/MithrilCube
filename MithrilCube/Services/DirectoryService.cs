94dd7b264145e6cebc287d2e0303a005615ee307
ginpay <y-toyoda@live.jp>
2021-11-27 13:27:22 +0900

    DeleteDirectoryは、Directory.Deleteでできるので削除

--- MithrilCube/Services/DirectoryService.cs
+++ MithrilCube/Services/DirectoryService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace MithrilCube.Services
@@ -29,11 +30,29 @@ namespace MithrilCube.Services
         /// <param name="extensions">検索する拡張子群{ ".cs", ".exe"}みたいな感じ</param>
         public void FolderInsiteSearch(string folderPath, List<string> fileFullPathList, string[] extensions);
 
+        // Directory.Deleteの第2引数をtrueにすると同じことができるので削除
+
+        ///// <summary>
+        ///// 指定したディレクトリの中身を全て削除する
+        ///// </summary>
+        ///// <param name="directory">ディレクトリ</param>
+        //public void DeleteDirectory(string directory);
+
         /// <summary>
-        /// 指定したディレクトリとその中身を全て削除する
+        /// 名前を指定して、アセンブリに埋め込まれているリソースを出力する
         /// </summary>
-        /// <param name="directory">ディレクトリ</param>
-        public void DeleteDirectory(string directory);
+        /// <param name="assambly">呼び出し元のアセンブリ情報</param>
+        /// <param name="resourceName">AssemblyのGetManifestResourceNamesで取得したリソース名</param>
+        /// <param name="basePath">出力先ディレクトリ</param>
+        public void CopyResource(Assembly assambly, string resourceName, string basePath = "./");
+
+        /// <summary>
+        /// アセンブリに埋め込まれているリソースを全て出力する
+        /// フォルダ構成も埋め込んだ時の構成の通りにする
+        /// </summary>
+        /// <param name="assambly">呼び出し元のアセンブリ情報</param>
+        /// <param name="basePath">出力先ディレクトリ</param>
+        public void CopyResources(Assembly assambly, string basePath = "./");
 
         ///// <summary>
         ///// ファイルを対象のディレクトリにコピーします
@@ -80,53 +99,92 @@ namespace MithrilCube.Services
                     FolderInsiteSearch(dirName, filenameList, extensions);
         }
 
-        public void DeleteDirectory(string directory)
-        {
-            // 再帰処理
-            DeleteDirectory(directory, true);
-        }
-
-        // 外から呼び出すときはisTop=trueとする
-        private void DeleteDirectory(string directory, bool isTop)
-        {
-            if (!Directory.Exists(directory))
-            {
-                return;
-            }
-
-            //ディレクトリ以外の全ファイルを削除
-            string[] filePaths = Directory.GetFiles(directory);
-            foreach (string filePath in filePaths)
-            {
-                File.SetAttributes(filePath, FileAttributes.Normal);
-                File.Delete(filePath);
-            }
-
-            //ディレクトリの中のディレクトリも再帰的に削除
-            string[] directoryPaths = Directory.GetDirectories(directory);
-            foreach (string directoryPath in directoryPaths)
-            {
-                DeleteDirectory(directoryPath, false);
-            }
-
-            if (!isTop)
-            {
-                //中が空になったらディレクトリ自身も削除
-                Directory.Delete(directory, false);
-            }
-        }
-
-        ///// <summary>
-        ///// 指定したディレクトリの中身を全て削除する
-        ///// （更に中身のあるフォルダがある場合の動作を確認してないので没）
-        ///// </summary>
-        //public void DeleteFiles(string directory)
+        //public void DeleteDirectory(string directory)
         //{
-        //    DirectoryInfo target = new DirectoryInfo(directory);
-        //    foreach (FileInfo file in target.GetFiles())
+        //    // 再帰処理
+        //    DeleteDirectorySub(directory, true);
+        //}
+
+        //// 外から呼び出すときはisTop=trueとする
+        //private void DeleteDirectorySub(string directory, bool isTop)
+        //{
+        //    if (!Directory.Exists(directory))
         //    {
-        //        file.Delete();
+        //        return;
+        //    }
+
+        //    //ディレクトリ以外の全ファイルを削除
+        //    string[] filePaths = Directory.GetFiles(directory);
+        //    foreach (string filePath in filePaths)
+        //    {
+        //        File.SetAttributes(filePath, FileAttributes.Normal);
+        //        File.Delete(filePath);
+        //    }
+
+        //    //ディレクトリの中のディレクトリも再帰的に削除
+        //    string[] directoryPaths = Directory.GetDirectories(directory);
+        //    foreach (string directoryPath in directoryPaths)
+        //    {
+        //        DeleteDirectorySub(directoryPath, false);
+        //    }
+
+        //    if (!isTop)
+        //    {
+        //        //中が空になったらディレクトリ自身も削除
+        //        Directory.Delete(directory, false);
         //    }
         //}
+
+        public void CopyResources(Assembly assambly, string basePath = "./")
+        {
+            // アセンブリに埋め込まれているすべてのリソースをコピー
+            foreach (var resourceName in assambly.GetManifestResourceNames())
+            {
+                // これは除外する
+                if (resourceName == $"{assambly.GetName().Name}.g.resources")   // "DigitalMegaFlareOffline.Modules.Common.g.resources"
+                {
+                    continue;
+                }
+
+                // アセンブリに埋め込まれているリソースを出力する
+                CopyResource(assambly, resourceName, basePath);
+            }
+        }
+
+        public void CopyResource(Assembly assambly, string resourceName, string basePath = "./")
+        {
+            // アセンブリに埋め込まれているリソースのStreamを取得する
+            using (var resourceStream = assambly.GetManifestResourceStream(resourceName)) // "DigitalMegaFlareOffline.Modules.Common.Sample.Excel.Sample1.xlsx"
+            {
+                var assamblyLength = assambly.GetName().Name.Length + 1;
+                var resourcePath = resourceName.Substring(assamblyLength, resourceName.Length - assamblyLength);        // Sample.Excel.Sample1.xlsx
+
+                // Sample/Excel/Sample1.xlsx にする
+                var splited = resourcePath.Split('.');
+                var fileName = $"{splited[splited.Length - 2]}.{splited[splited.Length - 1]}";
+                var filePath = string.Empty;
+                for (int i = 0; i < splited.Length - 2; i++)
+                {
+                    filePath += splited[i];
+                    filePath += "/";
+                }
+
+                // ディレクトリを作成する
+                SafeCreateDirectory(Path.Combine(basePath, filePath));
+
+                // 出力ストリーム
+                var fileFullPath = Path.Combine(basePath, filePath, fileName);
+                var outStream = new FileStream(fileFullPath, FileMode.Create);    // ファイルが存在していたら強制上書き
+
+                // リソースの内容を出力ストリームに書きだす
+                int b;
+                while ((b = resourceStream.ReadByte()) != -1)
+                {
+                    outStream.WriteByte((byte)b);
+                }
+                outStream.Close();
+            }
+        }
+
     }
 }
